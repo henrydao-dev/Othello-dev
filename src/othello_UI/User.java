602400b2dabe51ebeacad2e7c36d81c3f8669e64
Devin Prejean <devin.prejean@gmail.com>
2020-09-21 12:41:26 -0500

    reads the CSV!

--- src/othello_UI/User.java
+++ src/othello_UI/User.java
@@ -1,7 +1,14 @@
 package othello_UI;
+
+import com.opencsv.bean.CsvBindByName;
+
 public class User {
+	@CsvBindByName
 	String Name;
+	@CsvBindByName
 	String Password;
+	@CsvBindByName
 	int Wins;
+	@CsvBindByName
 	int Losses;
 }
