b77030a339c23e1949b792bb4f1c3896058b1593
henrydao-dev <71233905+henrydao-dev@users.noreply.github.com>
2020-09-21 04:15:35 -0500

    Added 8x8 grid

--- src/othello_UI/UI_Prototype.java
+++ src/othello_UI/UI_Prototype.java
@@ -24,34 +24,61 @@ package othello_UI;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
-
+import java.awt.Label;
 
 public class UI_Prototype extends Application {
-	
+	private GridPane Gpane;
 	//Creates our Primary Stage
 	public void start(Stage primaryStage) {
 		//Everything in here is in our main stage
-			
+		
+		
 			//Creates our pane
 			Pane pane = new Pane();
+			Gpane = new GridPane();
 			pane.setPadding(new Insets(0,0,0,0));
 			
 			//Creates our scene
-			Scene scene = new Scene(pane,750,750);
+			Scene scene = new Scene(Gpane,750,750);
 			
 			//Specific code for layout goes here
 				
-				//add grid lines (horizontal and vertical) to create board square
+			//add grid lines (horizontal and vertical) to create board square
+			//creates a 8x8 green grid with black lines
+			
+			int k = 0;
+			int r = 0;
+			String[][] color={{"GREEN","GREEN"},{"GREEN","GREEN"}};
+			Gpane.setAlignment(Pos.CENTER);   
+
+			for(int k1 = 0; k1 < 8; k1++) {
+			if(r > 1)
+			r = 0;
+			for(int k2 = 0; k2 < 8; k2++) {
+			if(k > 1)
+			k = 0;
+			Rectangle r1 = new Rectangle(50,50);
+			r1.setStroke(Color.BLACK);
+			r1.setFill(Paint.valueOf(color[r][k]));
+			Gpane.add(r1,k1,k2);
+			k++;
+			}
+			r++;
+			}
+			
 				
 				//Needed buttons
 					//add "settings" button
@@ -68,6 +95,8 @@ public class UI_Prototype extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show(); 
 		}
+	
+	
 
 	public static void main(String[] args) {
 		launch(args);
