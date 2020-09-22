28473d013b25fdfd842b05b62116bb5dd38ed61d
djackson93 <Dakoyaj1293@gmail.com>
2020-09-21 20:23:10 -0500

    Added All player name,score, time boxes. also added Settings/pass button

--- src/othello_UI/UI_Prototype.java
+++ src/othello_UI/UI_Prototype.java
@@ -28,6 +28,7 @@ import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -36,7 +37,10 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
 import java.awt.Label;
 
 public class UI_Prototype extends Application {
@@ -52,7 +56,9 @@ public class UI_Prototype extends Application {
 			pane.setPadding(new Insets(0,0,0,0));
 			
 			//Creates our scene
-			Scene scene = new Scene(Gpane,750,750);
+			Scene scene = new Scene(pane,800,850);
+			
+			pane.getChildren().add(Gpane);
 			
 			//Specific code for layout goes here
 				
@@ -62,7 +68,7 @@ public class UI_Prototype extends Application {
 			int k = 0;
 			int r = 0;
 			String[][] color={{"GREEN","GREEN"},{"GREEN","GREEN"}};
-			Gpane.setAlignment(Pos.CENTER);   
+			//Gpane.setAlignment(Pos.CENTER);   
 
 			for(int k1 = 0; k1 < 8; k1++) {
 			if(r > 1)
@@ -70,7 +76,7 @@ public class UI_Prototype extends Application {
 			for(int k2 = 0; k2 < 8; k2++) {
 			if(k > 1)
 			k = 0;
-			Rectangle r1 = new Rectangle(50,50);
+			Rectangle r1 = new Rectangle(75,75);
 			r1.setStroke(Color.BLACK);
 			r1.setFill(Paint.valueOf(color[r][k]));
 			Gpane.add(r1,k1,k2);
@@ -79,15 +85,99 @@ public class UI_Prototype extends Application {
 			r++;
 			}
 			
+			Gpane.setLayoutX(100);
+			Gpane.setLayoutY(150);
+			
+			
 				
 				//Needed buttons
-					//add "settings" button
+					//add "settings" button (will be replaced with a more appropriate "settings" icon later)
+					Text settings = new Text(745,30, "settings");
+					settings.setFill(Color.WHITE);
+					Rectangle settingsBox = new Rectangle();
+					settingsBox.setFill(Color.GRAY);
+					settingsBox.setWidth(50);
+					settingsBox.setHeight(50);
+					settingsBox.setLayoutX(740);
+					settingsBox.setLayoutY(5);
+					pane.getChildren().add(settingsBox);
+					pane.getChildren().add(settings);
+					
+			
 					//add "pass" button
+					Text pass = new Text(387, 95, "PASS");
+					Circle passButton = new Circle();
+					passButton.setFill(Color.RED);
+					passButton.setCenterX(402.5);
+					passButton.setCenterY(90);
+					passButton.setRadius(40);
+					pane.getChildren().add(passButton);
+					pane.getChildren().add(pass);
 			
 				//Text boxes needed
 					//Add "Player 1" and "Player 2" text boxes
+						//Player1
+					Text player1 = new Text(155,90, "Player 1: ");
+					TextField player1Box = new TextField();
+					player1Box.setMaxWidth(100);
+					player1Box.setLayoutX(130);
+					player1Box.setLayoutY(100);
+					pane.getChildren().add(player1);
+					pane.getChildren().add(player1Box);
+				
+						//Player2
+					Text player2 = new Text(600,90, "Player 2: ");
+					TextField player2Box = new TextField();
+					player2Box.setMaxWidth(100);
+					player2Box.setLayoutX(575);
+					player2Box.setLayoutY(100);
+					pane.getChildren().add(player2);
+					pane.getChildren().add(player2Box);
+				
 					//add player 1 and player 2 score boxes
+						//Player1
+					Text p1Score = new Text(255,90, "SCORE");
+					TextField p1ScoreBox = new TextField();
+					p1ScoreBox.setMaxWidth(50);
+					p1ScoreBox.setLayoutX(250);
+					p1ScoreBox.setLayoutY(100);
+					pane.getChildren().add(p1Score);
+					pane.getChildren().add(p1ScoreBox);
+				
+						//Player2
+					Text p2Score = new Text(510,90, "SCORE");
+					TextField p2ScoreBox = new TextField();
+					p2ScoreBox.setMaxWidth(50);
+					p2ScoreBox.setLayoutX(505);
+					p2ScoreBox.setLayoutY(100);
+					pane.getChildren().add(p2Score);
+					pane.getChildren().add(p2ScoreBox);
+					
 					//add player 1 and player 2 timer boxes
+						//Player1
+					Text placeHolderTime = new Text(275,39, "Time1");
+					placeHolderTime.setFill(Color.WHITE);
+					Rectangle p1TimerBox = new Rectangle();
+					p1TimerBox.setFill(Color.DARKBLUE);
+					p1TimerBox.setWidth(100);
+					p1TimerBox.setHeight(30);
+					p1TimerBox.setLayoutX(240);
+					p1TimerBox.setLayoutY(20);
+					pane.getChildren().add(p1TimerBox);
+					pane.getChildren().add(placeHolderTime);
+					
+					
+						//Player2
+					Text placeHolderTime2 = new Text(495,39, "Time2");
+					placeHolderTime2.setFill(Color.WHITE);
+					Rectangle p2TimerBox = new Rectangle();
+					p2TimerBox.setFill(Color.DARKBLUE);
+					p2TimerBox.setWidth(100);
+					p2TimerBox.setHeight(30);
+					p2TimerBox.setLayoutX(460);
+					p2TimerBox.setLayoutY(20);
+					pane.getChildren().add(p2TimerBox);
+					pane.getChildren().add(placeHolderTime2);
 			
 			
 			
@@ -96,7 +186,7 @@ public class UI_Prototype extends Application {
 			primaryStage.show(); 
 		}
 	
-	
+	//Idea: Set boolean flag for for player turns (if checked True, then player1 turn, if false, player2 turn)
 
 	public static void main(String[] args) {
 		launch(args);
