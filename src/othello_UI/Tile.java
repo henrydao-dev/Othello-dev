4f15b76980b43e09f28fb3bfff7b3b477754710a
Devin Prejean <devin.prejean@gmail.com>
2020-10-26 21:17:32 -0500

    Working clicks

---
+++ src/othello_UI/Tile.java
@@ -0,0 +1,15 @@
+package othello_UI;
+
+import javafx.scene.shape.Rectangle;
+
+public class Tile extends Rectangle {
+
+	public int row;
+	public int col;
+	
+	public Tile(double width, double height, int row, int col) {
+		super(width, height);
+		this.row = row;
+		this.col = col;
+	}
+}
